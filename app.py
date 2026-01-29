38d92772216fdbe6e1d3654803f9c697bab643ad
ammu2244 <rakshithad819@gmail.com>
2026-01-29 09:30:42 +0530

    first commit

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ def text_to_speech_file():
         return jsonify({'error': f'Text-to-speech error: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
